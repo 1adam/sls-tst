d873dfe950d90532354d47db9c25cf48e37f2d8a
1adam <1adam@users.noreply.github.com>
2018-12-16 05:18:29 +0000

    outp event

--- handler.js
+++ handler.js
@@ -3,8 +3,7 @@
 module.exports.HTTPendpoint = ( event, context, callback ) => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify({ message: `Hello, the time is ${new Date().toTimeString() }`,
-  }),
+    body: JSON.stringify( event ),
   };
   callback( null, response );
 };
